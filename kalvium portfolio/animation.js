1dc01645a399d4315bae92725d16e3b6f8d9367a
ADZY11 <aditi.singh@kalvium.community>
2024-12-30 12:49:34 +0530

    modified code

---
+++ kalvium portfolio/animation.js
@@ -0,0 +1,11 @@
+
+const card = document.querySelector('.card');
+
+setTimeout(() => {
+  card.classList.add('zoomed');
+}, 500); 
+
+
+setTimeout(() => {
+  window.location.href = 'index.html'; 
+}, 5000); 
